a09915c32f4624d59e7664d7604def10227f0cc9
sheronfdo <jamithsheron5@gmail.com>
2022-10-08 02:53:01 +0530

    initialize the project

---
+++ src/main/java/com/sheronfdp/studentmngstm/StudentMngstmApplication.java
@@ -0,0 +1,13 @@
+package com.sheronfdp.studentmngstm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMngstmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentMngstmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sheronfdp/studentmngstm/StudentMngstmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheronfdp.studentmngstm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMngstmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
