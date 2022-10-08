beb2f67188789047be0cfd54bf4561b080905071
sheronfdo <jamithsheron5@gmail.com>
2022-10-09 01:34:24 +0530

    Update StudentMngstmApplication.java

--- src/main/java/com/sheronfdp/studentmngstm/StudentMngstmApplication.java
+++ src/main/java/com/sheronfdp/studentmngstm/StudentMngstmApplication.java
@@ -3,24 +3,25 @@ package com.sheronfdp.studentmngstm;
 import com.sheronfdp.studentmngstm.entity.Student;
 import com.sheronfdp.studentmngstm.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentMngstmApplication {
+public class StudentMngstmApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentMngstmApplication.class, args);
     }
+
 
     @Autowired
     private StudentRepository studentRepository;
 
     @Override
     public void run(String... args) throws Exception{
-        Student student1 = new Student("Ramesh", "FADATARE", "rasmesh@gmail,com");
-        studentRepository.save(student1);
+        //Student student1 = new Student("Ramesh", "FADATARE", "rasmesh@gmail,com");
+        //studentRepository.save(student1);
 
     }
-
 }
