f3317f29df24b97ead60358fef9260a6e052ae66
sheronfdo <jamithsheron5@gmail.com>
2022-10-12 08:31:44 +0530

    all processes completed
    all processes completed insert, delete, update

--- src/main/java/com/sheronfdp/studentmngstm/controller/StudentController.java
+++ src/main/java/com/sheronfdp/studentmngstm/controller/StudentController.java
@@ -5,6 +5,9 @@ import com.sheronfdp.studentmngstm.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -16,15 +19,48 @@ public class StudentController {
     }
 
     @GetMapping("/students")
-    public String listStudents(Model model){
-        model.addAttribute("students",studentService.getAllStudents());
+    public String listStudents(Model model) {
+        model.addAttribute("students", studentService.getAllStudents());
         return "students";
     }
 
     @GetMapping("students/new")
-    public String createStudentForm(Model model){
+    public String createStudentForm(Model model) {
         Student student = new Student();
         model.addAttribute("student", student);
         return "create_student";
     }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("student") Student student) {
+        studentService.saveStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String editStudentForm(@PathVariable Long id, Model model) {
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "edit_student";
+    }
+
+    @PostMapping("/students/{id}")
+    public String updateStudent(@PathVariable Long id,
+                                @ModelAttribute("student") Student student,
+                                Model model) {
+        Student exsistingStudent = studentService.getStudentById(id);
+        exsistingStudent.setId(id);
+        exsistingStudent.setFirstName(student.getFirstName());
+        exsistingStudent.setLastName(student.getLastName());
+        exsistingStudent.setEmail(student.getEmail());
+
+        studentService.updateStudent(exsistingStudent);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/{id}")
+    public String deleteStudent(@PathVariable Long id){
+        studentService.deleteStudentById(id);
+        return "redirect:/students";
+    }
+
 }
